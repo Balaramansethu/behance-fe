8f82fc6eee76e2e514ba6a3b4b712925f8719ed3
Balaramansethu <baalaaraman@gmail.com>
2024-06-20 10:05:14 +0530

    Initial commit for file structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeComponent from './components/HomeComponent/HomeComponent'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <HomeComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/components/HomeComponent/HomeComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeComponent = () => {
+  return (
+    <div>
+      This is skeleton structure !!
+    </div>
+  )
+}
+
+export default HomeComponent
