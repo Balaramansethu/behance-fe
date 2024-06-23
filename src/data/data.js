c6ef0e98ce939ccad9177935a38d876c6894fb69
Balaramansethu <baalaaraman@gmail.com>
2024-06-23 23:40:27 +0530

    fix. fixed some ui bugs in homecomponent

--- src/data/data.js
+++ src/data/data.js
@@ -11,17 +11,17 @@ import image10 from "../assets/images/img10.png";
 import image11 from "../assets/images/img11.png";
 import image12 from "../assets/images/img12.png";
 const images = [
-  { src: image1, author: "Hincha", likes: 30, views: 1.2 },
-  { src: image2, author: "Posterlife", likes: 70, views: 4.2 },
+  { src: image1, author: "Hinchain", likes: 30, views: 1.2 },
+  { src: image2, author: "lalaregy", likes: 70, views: 4.2 },
   { src: image3, author: "Pickpak", likes: 20, views: 1.6 },
-  { src: image4, author: "Niturs", likes: 10, views: 7.3 },
+  { src: image4, author: "Niturs:)", likes: 10, views: 7.3 },
   { src: image5, author: "Falhead", likes: 85, views: 8.3 },
   { src: image6, author: "Greham", likes: 121, views: 9.2 },
   { src: image7, author: "Goldkins", likes: 74, views: 4.7 },
   { src: image8, author: "Torvi", likes: 96, views: 6.6 },
-  { src: image9, author: "Valahala", likes: 36, views: 1.9 },
+  { src: image9, author: "Valhala", likes: 36, views: 1.9 },
   { src: image10, author: "Nixo", likes: 41, views: 5.3 },
   { src: image11, author: "Detunty", likes: 51, views: 2.2 },
-  { src: image12, author: "Tedbundy", likes: 66, views: 3.9 },
+  { src: image12, author: "Tedfe", likes: 66, views: 3.9 },
 ];
 export default images;
